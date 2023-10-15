1233f11bcb964a15a3e2f69fd01b52878568ecd7
mjhalwa <mjhalwa.codes@gmail.com>
2023-10-15 22:13:01 +0200

    refactor: uses separate subfunctions to manipulate search result table

--- scripts/highlight_search_results.js
+++ scripts/highlight_search_results.js
@@ -1,3 +1,74 @@
+function isHeaderRow(row) {
+  return (row.className === "list_head")
+}
+
+function get_row_usdb_id(row) {
+  let usdb_id = -1
+  if (!isHeaderRow(row)) {
+    const first_original_column = row.children[0]
+    const attr_onclick = first_original_column.attributes["onclick"]
+    usdb_id = Number(attr_onclick.textContent.replace(/^[^0-9]*([0-9]+)[^0-9]*$/, "$1"))
+  }
+  return usdb_id
+}
+
+function prependIdColumn(row, usdb_id) {
+  const first_original_column = row.children[0]
+  if (isHeaderRow(row)) {
+    const th_usdb_id = document.createElement("td");  //!< Note: usdb uses <td> for heads
+    th_usdb_id.baseURI = first_original_column.baseURI
+    const order_link = document.createElement("a");
+    order_link.href = first_original_column.children[0].href.replace("&order=interpret","&order=id")
+    if (window.location.href.includes("&order=id")) {
+      order_link.href += (window.location.href.includes("&ud=asc")) ? "&ud=desc" : "&ud=asc"
+    } else {
+      order_link.href = order_link.href.replace(/&ud=(?:desc|asc)/, "" )
+      order_link.href += "&ud=asc"
+    }
+    order_link.textContent = "ID"
+    order_link.style = "color: #FFFFFF;text-decoration: none;"
+    th_usdb_id.appendChild(order_link)
+    if (window.location.href.includes("&order=id")) {
+      if (window.location.href.includes("&ud=asc")) {
+      } if (window.location.href.includes("&ud=desc")) {
+        order_img = document.createElement("img")
+        order_img.alt="absteigender"
+        order_img.src="images/down_small.png"
+        th_usdb_id.appendChild(order_img)
+      } else {
+        order_img = document.createElement("img")
+        order_img.alt="aufsteigender"
+        order_img.src="images/up_small.png"
+        th_usdb_id.appendChild(order_img)
+      }
+    }
+    row.insertBefore(th_usdb_id, first_original_column)
+
+  } else {
+    // insert column with usdb ID
+    const td_usdb_id = document.createElement("td");
+    td_usdb_id.attributes = first_original_column.attributes
+    td_usdb_id.textContent = usdb_id.toString()
+    row.insertBefore(td_usdb_id, first_original_column)
+  }
+}
+
+/**
+ * apply row background color based on configuration and usdb_id
+ * @param {*} row 
+ * @param {*} usdb_id 
+ */
+function highlight_row(row, usdb_config, usdb_id) {
+  if (!isHeaderRow(row)) {
+    for (let conf of usdb_config) {
+      if (conf.ids.includes(usdb_id)) {
+        row.style["background-color"] = `${conf.color}`
+        break
+      }
+    }
+  }
+}
+
 browser.storage.sync.get().then( sync_storage => {
   console.log(sync_storage)
   if ("config" in sync_storage) {
@@ -6,58 +77,9 @@ browser.storage.sync.get().then( sync_storage => {
     const result_rows = result_table.getElementsByTagName("tr")
             
     for ( let i=0; i<result_rows.length; i++ ) {
-      const first_original_column = result_rows[i].children[0]
-      if (result_rows[i].className === "list_head") {
-        // Table Head
-        // insert head for column with usdb ID
-        const th_usdb_id = document.createElement("td");  //!< Note: usdb uses <td> for heads
-        th_usdb_id.baseURI = first_original_column.baseURI
-        const order_link = document.createElement("a");
-        order_link.href = first_original_column.children[0].href.replace("&order=interpret","&order=id")
-        if (window.location.href.includes("&order=id")) {
-          order_link.href += (window.location.href.includes("&ud=asc")) ? "&ud=desc" : "&ud=asc"
-        } else {
-          order_link.href = order_link.href.replace(/&ud=(?:desc|asc)/, "" )
-          order_link.href += "&ud=asc"
-        }
-        order_link.textContent = "ID"
-        order_link.style = "color: #FFFFFF;text-decoration: none;"
-        th_usdb_id.appendChild(order_link)
-        if (window.location.href.includes("&order=id")) {
-          if (window.location.href.includes("&ud=asc")) {
-          } if (window.location.href.includes("&ud=desc")) {
-            order_img = document.createElement("img")
-            order_img.alt="absteigender"
-            order_img.src="images/down_small.png"
-            th_usdb_id.appendChild(order_img)
-          } else {
-            order_img = document.createElement("img")
-            order_img.alt="aufsteigender"
-            order_img.src="images/up_small.png"
-            th_usdb_id.appendChild(order_img)
-          }
-        }
-        result_rows[i].insertBefore(th_usdb_id, first_original_column)
-
-      } else {
-        // Table Row
-        const attr_onclick = first_original_column.attributes["onclick"]
-        const usdb_id = Number(attr_onclick.textContent.replace(/^[^0-9]*([0-9]+)[^0-9]*$/, "$1"))
-
-        // insert column with usdb ID
-        const td_usdb_id = document.createElement("td");
-        td_usdb_id.attributes = first_original_column.attributes
-        td_usdb_id.textContent = usdb_id.toString()
-        result_rows[i].insertBefore(td_usdb_id, first_original_column)
-
-        // apply row background color based on configuration and usdb_id
-        for (let conf of usdb_config) {
-          if (conf.ids.includes(usdb_id)) {
-            result_rows[i].style["background-color"] = `${conf.color}`
-            break
-          }
-        }
-      }
+      const usdb_id = get_row_usdb_id(result_rows[i])
+      prependIdColumn(result_rows[i], usdb_id)
+      highlight_row(result_rows[i], usdb_config, usdb_id)
     }
   } else {
     console.log("empty config!")
