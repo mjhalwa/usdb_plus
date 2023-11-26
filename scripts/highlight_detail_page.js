06e216654967c03673a71da3cd1d14dad272c895
mjhalwa <mjhalwa.codes@gmail.com>
2023-11-26 22:37:38 +0100

    fix: sets default config on all pages if no config is present yet, additionally reduces to single default category, fixes #2

--- scripts/highlight_detail_page.js
+++ scripts/highlight_detail_page.js
@@ -1,7 +1,43 @@
-browser.storage.sync.get().then( sync_storage => {
-  console.log(sync_storage)
+// UTILITY FUNCTIONS
+
+function get_config_default() {
+  // console.log("get_config_default")
+  return {
+    "general": {
+      "prepend_id_column": true,
+      "remove_on_click": true,
+      "categories_url": ""
+    },
+    "categories": [
+      {"label": "My Songs", "color": "#C3EDC0", "ids": []}
+    ]
+  }
+}
+
+function get_config_or_set_default(sync_storage) {
+  // default settings
+  let usdb_config = {...get_config_default()}
   if ("config" in sync_storage) {
-    const usdb_config = sync_storage["config"]
+    // retrieve settings from sync storage
+    usdb_config = sync_storage["config"]
+    // transition from v0.1.0 to v1.0.0
+    if(Array.isArray(usdb_config)) {
+      usdb_config = {...get_config_default(), "categories": usdb_config}
+      browser.storage.sync.set({"config": usdb_config})
+    }
+  } else {
+    // set default settings
+    browser.storage.sync.set({"config": usdb_config})
+  }
+  return usdb_config
+}
+
+// 
+//
+//
+
+browser.storage.sync.get().then( sync_storage => {
+    usdb_config = get_config_or_set_default(sync_storage)
     const usdb_id = Number(new URLSearchParams(document.URL).get("id"))
     // console.log(usdb_id)
     const details_section = document.getElementById("tablebg")
@@ -20,7 +56,5 @@ browser.storage.sync.get().then( sync_storage => {
         break
       }
     }
-  } else {
-    console.log("empty config!")
   }
-})
+)
